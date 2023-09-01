c29b1628fb811039f6d12fba5e06500f6ed77394
DerekLinan <4261674+DerekLinan@users.noreply.github.com>
2023-09-01 04:51:00 -0600

    feat(CourseraCourseCard): style

--- frontend/src/data/courses/Coursera.ts
+++ frontend/src/data/courses/Coursera.ts
@@ -1,23 +1,23 @@
 import { Course } from './types';
 
-export type LinkedInCourses = {
-  courses: LinkedInCourse[];
+export type CourseraCourses = {
+  courses: CourseraCourse[];
 };
 
-export type LinkedInCourse = Course & {
+export type CourseraCourse = Course & {
   company: string;
   companyImage: string;
   credentialID: string;
   credentialURL: URL;
 };
 
-export const LINKEDINCOURSES: LinkedInCourses = {
+export const COURSERACOURSES: CourseraCourses = {
   courses: [
     {
       name: 'Introduction to Software Engineering',
       date: new Date(2023, 8),
       company: 'IBM',
-      companyImage: 'ibm-logo.png',
+      companyImage: 'ibm-logo.svg',
       credentialID: 'DLBUPB72KUXR',
       credentialURL: new URL(
         'https://www.coursera.org/account/accomplishments/certificate/DLBUPB72KUXR',
@@ -27,7 +27,7 @@ export const LINKEDINCOURSES: LinkedInCourses = {
       name: 'Hands-on Introduction to Linux Commands and Shell Scripting',
       date: new Date(2023, 8),
       company: 'IBM',
-      companyImage: 'ibm-logo.png',
+      companyImage: 'ibm-logo.svg',
       credentialID: 'LM4LBYDAZNL7',
       credentialURL: new URL(
         'https://www.coursera.org/account/accomplishments/certificate/LM4LBYDAZNL7',
